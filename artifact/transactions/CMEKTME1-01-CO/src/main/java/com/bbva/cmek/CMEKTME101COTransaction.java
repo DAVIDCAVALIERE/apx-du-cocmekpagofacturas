f09e040ef7385a85a0d486079ef03ec1e5b2782c
cavaliere <davincicavaliere@gmail.com>
2025-06-20 08:29:33 -0500

    feat: creacion de transaccion con su respectivo TEST

--- artifact/transactions/CMEKTME1-01-CO/src/main/java/com/bbva/cmek/CMEKTME101COTransaction.java
+++ artifact/transactions/CMEKTME1-01-CO/src/main/java/com/bbva/cmek/CMEKTME101COTransaction.java
@@ -1,24 +1,48 @@
+
 package com.bbva.cmek;
 
+import com.bbva.cmek.dto.bill.PaymentDTO;
 import com.bbva.cmek.lib.rme1.CMEKRME1;
+import com.bbva.elara.domain.transaction.Severity;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
  * Invoice payment transaction charged to account.
- *
  */
 public class CMEKTME101COTransaction extends AbstractCMEKTME101COTransaction {
 
-	private static final Logger LOGGER = LoggerFactory.getLogger(CMEKTME101COTransaction.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(CMEKTME101COTransaction.class);
 
-	/**
-	 * The execute method...
-	 */
-	@Override
-	public void execute() {
-		CMEKRME1 cmekRME1 = this.getServiceLibrary(CMEKRME1.class);
-		// TODO - Implementation of business logic
-	}
+    /**
+     * The execute method...
+     */
+    @Override
+    public void execute() {
+        //Definicion por defecto que hace la arquitectura al agregar la dependencia de la libreria
+        CMEKRME1 cmekRME1 = this.getServiceLibrary(CMEKRME1.class);
+
+        //Obtener la variable payment que se definio como entrada de la transaccion y se homologa
+        //al objeto de entrada del metodo de la libreria a utiliza. No se requiere validacion sobre
+        //los datos de la entrada de la transaccion porque se supone ya la hizo la arquitectura.
+        //Todos los parametros de entrada de la transaccion que estan en la raiz / tiene un get
+        PaymentDTO request = getPayment();
+
+        //Ejecutar el metodo de la libreria y recibir la respuesta del metodo.
+        PaymentDTO response = cmekRME1.executeDoBillPayment(request);
+
+        //Se sugiere incluir la siguiente validacion para manejar la respuesta y la severidad o estatus
+        //Si hay algun advice o error controlado se genera una respuesta con error, que retornara los
+        //codigos configurado
+        if (!getAdviceList().isEmpty()) {
+            //Si se requiere una logica adicional se incluye aca
+            this.setSeverity(Severity.ENR);
+        } else {
+            //Si no hay errores, se homologa la respuesta de la libreria a la salida de la transaccion
+            //Todos los parametros de salida de la transaccion que estan en la raiz / tiene un set
+            setPayment(response);
+        }
+    }
 
 }
+
--- artifact/transactions/CMEKTME1-01-CO/src/test/java/com/bbva/cmek/CMEKTME101COTransactionTest.java
+++ artifact/transactions/CMEKTME1-01-CO/src/test/java/com/bbva/cmek/CMEKTME101COTransactionTest.java
@@ -1,6 +1,9 @@
 package com.bbva.cmek;
 
+import com.bbva.cmek.dto.bill.PaymentDTO;
+import com.bbva.cmek.lib.rme1.CMEKRME1;
 import com.bbva.elara.configuration.manager.application.ApplicationConfigurationService;
+import com.bbva.elara.domain.transaction.Advice;
 import com.bbva.elara.domain.transaction.Context;
 import com.bbva.elara.domain.transaction.RequestHeaderParamsName;
 import com.bbva.elara.domain.transaction.request.header.CommonRequestHeader;
@@ -21,78 +24,95 @@ import static org.mockito.Mockito.when;
 
 public class CMEKTME101COTransactionTest {
 
-	private Map<String, Object> parameters;
+    private Map<String, Object> parameters;
 
-	private Map<Class<?>, Object> serviceLibraries;
+    private Map<Class<?>, Object> serviceLibraries;
 
-	@Mock
-	private ApplicationConfigurationService applicationConfigurationService;
+    @Mock
+    private ApplicationConfigurationService applicationConfigurationService;
 
-	@Mock
-	private CommonRequestHeader commonRequestHeader;
+    @Mock
+    private CommonRequestHeader commonRequestHeader;
 
-	private final CMEKTME101COTransaction transaction = new CMEKTME101COTransaction() {
-		@Override
-		protected void addParameter(String field, Object obj) {
-			if (parameters != null) {
-				parameters.put(field, obj);
-			}
-		}
+    //Toda libreria que se use en la transaccion debe definirse como mock
+    @Mock
+    private CMEKRME1 cmekRME1;
 
-		@Override
-		protected Object getParameter(String field) {
-			return parameters.get(field);
-		}
+    private final CMEKTME101COTransaction transaction = new CMEKTME101COTransaction() {
+        @Override
+        protected void addParameter(String field, Object obj) {
+            if (parameters != null) {
+                parameters.put(field, obj);
+            }
+        }
 
-		@Override
-		protected <T> T getServiceLibrary(Class<T> serviceInterface) {
-			return (T) serviceLibraries.get(serviceInterface);
-		}
+        @Override
+        protected Object getParameter(String field) {
+            return parameters.get(field);
+        }
 
-		@Override
-		public String getProperty(String keyProperty) {
-			return applicationConfigurationService.getProperty(keyProperty);
-		}
+        @Override
+        protected <T> T getServiceLibrary(Class<T> serviceInterface) {
+            return (T) serviceLibraries.get(serviceInterface);
+        }
 
-		@Override
-		protected CommonRequestHeader getRequestHeader() {
-		 	return commonRequestHeader;
-		}
-	};
+        @Override
+        public String getProperty(String keyProperty) {
+            return applicationConfigurationService.getProperty(keyProperty);
+        }
 
-	@Before
-	public void setUp() throws Exception {
-		MockitoAnnotations.initMocks(this);
-		initializeTransaction();
-		// TODO - Set the mocks created from the libraries to the transaction, e.g.:
-		// setServiceLibrary(QWAIR001.class, qwaiR001);
-	}
+        @Override
+        protected CommonRequestHeader getRequestHeader() {
+            return commonRequestHeader;
+        }
+    };
 
-	private void initializeTransaction() {
-		transaction.setContext(new Context());
-		parameters = new HashMap<>();
-		serviceLibraries = new HashMap<>();
-	}
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+        initializeTransaction();
+        // Set the mocks created from the libraries to the transaction, e.g.:
+        // setServiceLibrary(QWAIR001.class, qwaiR001);
+        //Toda libreria que se defina como mock debe incluirse con la siguiente linea
+        setServiceLibrary(CMEKRME1.class, cmekRME1);
+    }
 
-	private void setServiceLibrary(Class<?> clasz, Object mockObject) {
-		serviceLibraries.put(clasz, mockObject);
-	}
+    private void initializeTransaction() {
+        transaction.setContext(new Context());
+        parameters = new HashMap<>();
+        serviceLibraries = new HashMap<>();
+    }
 
-	private void setParameterToTransaction(String parameter, Object value) {
-		parameters.put(parameter, value);
-	}
+    private void setServiceLibrary(Class<?> clasz, Object mockObject) {
+        serviceLibraries.put(clasz, mockObject);
+    }
 
-	private Object getParameterFromTransaction(String parameter) {
-		return parameters.get(parameter);
-	}
+    private void setParameterToTransaction(String parameter, Object value) {
+        parameters.put(parameter, value);
+    }
 
-	@Test
-	public void executeTest() {
-		// when(commonRequestHeader.getHeaderParameter(RequestHeaderParamsName.COUNTRYCODE)).thenReturn("ES");
-		// when(applicationConfigurationService.getProperty("config.property")).thenReturn("value");
-		// when(qwaiR001.execute()).thenReturn(listCustomerDTO);
-		// setParameterToTransaction("customerIn", new CustomerDTO());
-		transaction.execute();
-		Assert.assertEquals(0, transaction.getAdviceList().size());
-	}
+    private Object getParameterFromTransaction(String parameter) {
+        return parameters.get(parameter);
+    }
+
+    @Test
+    public void executeTest() {
+        // when(commonRequestHeader.getHeaderParameter(RequestHeaderParamsName.COUNTRYCODE)).thenReturn("ES");
+        // when(applicationConfigurationService.getProperty("config.property")).thenReturn("value");
+        // when(qwaiR001.execute()).thenReturn(listCustomerDTO);
+        // setParameterToTransaction("customerIn", new CustomerDTO());
+
+        //Se define la entrada de la transaccion
+        setParameterToTransaction("payment", new PaymentDTO());
+
+        //Se ejecuta la transaccion
+        transaction.execute();
+        Assert.assertEquals(0, transaction.getAdviceList().size());
+
+        //Se define el camino cuando hay addvices
+        transaction.getAdviceList().add(new Advice());
+        //Se ejecuta la transaccion
+        transaction.execute();
+        Assert.assertEquals(1, transaction.getAdviceList().size());
+    }
 }
